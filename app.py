0dc14a1d28e321b2cd3a89dce01d6f1912a48257
PuChan-HCI <40685593+PuChan-HCI@users.noreply.github.com>
2022-07-06 09:51:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,29 @@
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer
-import av
 import cv2
+from PIL import Image
 
-st.title("My first Streamlit app")
-st.write("Hello, world")
+uploaded_video = st.file_uploader("Choose video", type=["mp4", "mov"])
+frame_skip = 300 # display every 300 frames
 
+if uploaded_video is not None: # run only when user uploads video
+    vid = uploaded_video.name
+    with open(vid, mode='wb') as f:
+        f.write(uploaded_video.read()) # save video to disk
 
-class VideoProcessor:
-    def __init__(self) -> None:
-        self.threshold1 = 100
-        self.threshold2 = 200
+    st.markdown(f"""
+    ### Files
+    - {vid}
+    """,
+    unsafe_allow_html=True) # display file name
 
-    def recv(self, frame):
-        img = frame.to_ndarray(format="bgr24")
+    vidcap = cv2.VideoCapture(vid) # load video from disk
+    cur_frame = 0
+    success = True
 
-        img = cv2.cvtColor(cv2.Canny(img, self.threshold1, self.threshold2), cv2.COLOR_GRAY2BGR)
-
-        return av.VideoFrame.from_ndarray(img, format="bgr24")
-
-
-ctx = webrtc_streamer(
-    key="example",
-    video_processor_factory=VideoProcessor,
-    rtc_configuration={
-        "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
-    }
-)
-if ctx.video_processor:
-    ctx.video_processor.threshold1 = st.slider("Threshold1", min_value=0, max_value=1000, step=1, value=100)
-    ctx.video_processor.threshold2 = st.slider("Threshold2", min_value=0, max_value=1000, step=1, value=200)
+    while success:
+        success, frame = vidcap.read() # get next frame from video
+        if cur_frame % frame_skip == 0: # only analyze every n=300 frames
+            print('frame: {}'.format(cur_frame)) 
+            pil_img = Image.fromarray(frame) # convert opencv frame (with type()==numpy) into PIL Image
+            st.image(pil_img)
+        cur_frame += 1
